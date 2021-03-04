8d58d27defe300df9b1da9d4ec377a0017ea70c8
Vladimir <mikhaylov-99@list.ru>
2021-03-04 10:43:10 +0400

    add project

---
+++ src/main/java/Bot.java
@@ -0,0 +1,67 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.io.IOException;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try{
+            telegramBotsApi.registerBot(new Bot());
+        }
+        catch (TelegramApiRequestException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try{
+            sendMessage(sendMessage);
+        }
+        catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onUpdateReceived(Update update) {
+        Model model = new Model();
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/setting":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                default:
+                    try {
+                        sendMsg(message, Weather.getWeather(message.getText(), model));
+                    } catch (IOException e) {
+                        sendMsg(message, "Город не найден!");
+                    }
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "Weather_MyBot";
+    }
+
+    public String getBotToken() {
+        return "1638864975:AAFz26nU7eOuo3ep6oXI04wTAXvb43uXYBA";
+    }
+}
